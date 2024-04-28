a7a95bf2bed1741c3ddb1047e2c817364419da6e
Jimit <jimitsoni18@gmail.com>
2024-04-28 23:02:09 +0530

    changed repository name so removed base, fixed styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://JimitSoni18.github.io/",
-	base: "portfolio",
 	integrations: [tailwind()],
 });
